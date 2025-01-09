e37a9fdf84b1cfadadc752885a2eaa4baa6d1444
Eden Shmuel <103032305+Edenshmuel@users.noreply.github.com>
2025-01-09 15:48:01 +0200

    Add files via upload

--- Prepare_data.py
+++ Prepare_data.py
@@ -9,7 +9,7 @@
 #Importing necessary libraries for data preprocessing
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import RobustScaler, LabelEncoder
+from sklearn.preprocessing import RobustScaler, LabelEncoder, OneHotEncoder
 
 
 # In[ ]:
@@ -87,10 +87,6 @@ def Preprocessing(datasets, train_mappings=None, scaler=None, fit_scaler=False):
     datasets['YeshuvCrimeRate'] = datasets.groupby('Yeshuv')['Yeshuv'].transform('count')
     datasets['CrimeTrend'] = datasets.groupby('Year')['Year'].transform('count')
 
-    # Adding a combined time index and a seasonal feature (Spring/Summer = 0, Autumn/Winter = 1)
-    datasets['TimeIndex'] = datasets['Year'] * 4 + datasets['Quarter_numeric']
-    datasets['Season'] = datasets['Quarter_numeric'].apply(lambda q: (q % 4) // 2)
-
     # Adding interaction features: CrimeTrend multiplied by YeshuvCrimeRate
     datasets['CrimeTrend_CrimeRate'] = datasets['CrimeTrend'] * datasets['YeshuvCrimeRate']
 
@@ -98,16 +94,13 @@ def Preprocessing(datasets, train_mappings=None, scaler=None, fit_scaler=False):
     station_crime_rate = datasets.groupby('PoliceStation')['YeshuvCrimeRate'].transform('mean')
     datasets['StationCrimeRateAvg'] = station_crime_rate
 
-    # Calculating number of crimes reported at each police station
-    datasets['PoliceStationCrimeCount'] = datasets.groupby('PoliceStation')['PoliceStation'].transform('count')
-
     # Calculating historical crime rate per Yeshuv
     datasets['YeshuvHistoricalCrimeRate'] = datasets.groupby('Yeshuv')['YeshuvCrimeRate'].transform('mean')
 
-    # Calculating the number of nearby police stations within the same district
+    # === Urban vs. Rural Classification ===
+     # Calculating the number of nearby police stations within the same district
     datasets['StationsNearbyCount'] = datasets.groupby('PoliceDistrict')['PoliceStation'].transform('nunique')
 
-    # === Urban vs. Rural Classification ===
     # Defining a list of cities in Hebrew for classification purposes
     hebrew_cities = [
         "אום אל-פחם", "אופקים", "אור יהודה", "אור עקיבא", "אילת", "אלעד", "אריאל",
